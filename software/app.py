47d9dc3cf291d60d1e1f546241d15a29f83859ba
zibbini <ziadibbini@gmail.com>
2024-03-05 12:50:58 +0000

    Add analogue gain to acquisition and live stream callbacks.

--- software/app.py
+++ software/app.py
@@ -51,7 +51,7 @@ long_callback_manager = DiskcacheLongCallbackManager(cache)
 
 # App specific modules
 import layout
-import camera_backend_picamera2 as picam2
+import camera_backend_picamera2
 import renderers
 import emails
 import leds
@@ -114,26 +114,6 @@ def check_devices():
     print(manual_control_port, xyz_port)
     return manual_control_port, xyz_port
 
-def check_camera():
-    proc = subprocess.Popen(['libcamera-hello', '--list-cameras'], stdout=subprocess.PIPE)
-    
-    imx_line = None
-    while True:
-        line = proc.stdout.readline()
-        if not line:
-            break
-        
-        line = str(line.rstrip())
-        if 'imx' in line:
-            imx_line = line
-    
-    if not imx_line:
-        raise SystemError('No cameras detected. Aborting startup seqeunce.')
-        
-    imx_id = imx_line.find('imx')
-    camera_model = imx_line[imx_id:imx_id+6]
-    return camera_model 
-
 # Source: https://gist.github.com/IdeaKing/11cf5e146d23c5bb219ba3508cca89ec
 def resize_with_pad(image, new_shape, padding_color=(0,0,0)):
     """Maintains aspect ratio and resizes with padding.
@@ -155,32 +135,25 @@ def resize_with_pad(image, new_shape, padding_color=(0,0,0)):
     image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=padding_color)
     return image
 
-def gen(camera_settings):
+def gen(camera, camera_settings):
     while True:
         if camera_state.trigger:
 
             exposure = camera_settings.settings['exposure']
             fps = camera_settings.settings['framerate']
             sensor_mode = camera_settings.settings['sensor_mode']
+            analogue_gain = camera_settings.settings['analogue_gain']
 
             if camera_state.state == 'streaming':
-                with picam2.VideoGenerator(exposure, fps, sensor_mode) as cap_gen:
+                with camera_backend_picamera2.VideoGenerator(camera, exposure, fps, analogue_gain, sensor_mode) as cap_gen:
                     for frame in cap_gen:
                         if camera_state.trigger:
                             frame = resize_with_pad(frame, (640, 480))
-                            frame = picam2.frame_to_bytes(frame)
+                            frame = camera_backend_picamera2.frame_to_bytes(frame)
                             yield (b'--frame\r\n'
                                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
                         else:
                             break
-
-            # Deprecated and unused ---------------------
-            elif camera_state.state == 'snapshot':
-                frame = picam2.still_capture()
-                frame = picam2.frame_to_bytes(frame)
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-                camera_state.trigger = False
         else:
             time.sleep(0.1)
 
@@ -323,17 +296,18 @@ class RelativeXYZ:
 
 relative = RelativeXYZ()
 
+picam2 = camera_backend_picamera2.Camera()
+
 manual_control_port, xyz_port = check_devices()
 sensor_modes = picam2.get_sensor_modes()
-camera_model = check_camera()
 
 # Initiate serial connections
 manual_control_serial = serial.Serial(manual_control_port, 115200)
 xyz_serial = serial.Serial(xyz_port, 115200)
 
 camera_state = CameraState()
-camera_settings = picam2.CameraSettings()
-live_stream = picam2.LiveStream()
+camera_settings = camera_backend_picamera2.CameraSettings()
+live_stream = camera_backend_picamera2.LiveStream(picam2.camera)
 
 experimental_log = ExperimentJobLog()
 acquisition = Acquisition()
@@ -382,6 +356,19 @@ def update_exposure_ui(cam_init, loaded_data):
     else:
         exposure = loaded_camera_settings.exposure / 1000 # For ms on pi
     return exposure
+
+@app.callback(
+    output=Output('analogue_gain', 'value'),
+    inputs=[
+        Input('connect-cam-callback', 'children'),
+        Input('loaded-data-callback', 'children')
+    ])
+def update_analogue_gain_ui(cam_init, loaded_data):
+    if trigger not in [loaded_data]:
+        analogue_gain = camera_settings.get('analogue_gain') # For ms on pi
+    else:
+        analogue_gain = loaded_camera_settings.analogue_gain # For ms on pi
+    return analogue_gain
 
 @app.callback(
     output=[Output('resolution-preset', 'value')],
@@ -440,20 +427,22 @@ def update_hardware_brightness(value):
         State('connect-cam-callback', 'children'),
         State('exposure', 'value'),
         State('resolution-preset', 'value'),
-        State('fps', 'value')
+        State('fps', 'value'),
+        State('analogue_gain', 'value')
     ])
-def update_camera_settings(n_clicks, cam_init, exposure, resolution, fps):
+def update_camera_settings(n_clicks, cam_init, exposure, resolution, fps, analogue_gain):
     if n_clicks:
         camera_settings.set('exposure', exposure * 1000) # Convert back to exposure units on pi
         camera_settings.set('sensor_mode', resolution)
         camera_settings.set('framerate', fps)
+        camera_settings.set('analogue_gain', analogue_gain)
         return trigger  
     else:
         return dash.no_update
 
 @server.route('/video_feed')
 def video_feed():
-    return Response(gen(camera_settings),
+    return Response(gen(picam2.camera, camera_settings),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 class TabState:
@@ -486,10 +475,11 @@ def start_stop_live_view(n_clicks, cam_init, mode):
                 exposure = camera_settings.settings['exposure']
                 fps = camera_settings.settings['framerate']
                 sensor_mode = camera_settings.settings['sensor_mode']
+                analogue_gain = camera_settings.settings['analogue_gain']
                 
                 print('SENSOR_MODE', sensor_mode)
                 
-                live_stream.start(exposure, fps, sensor_mode)
+                live_stream.start(exposure, fps, analogue_gain, sensor_mode)
                 return trigger, trigger, dbc.Spinner(color='primary', size='md')
             # Browser -----
             else:
@@ -524,8 +514,9 @@ def update_still_image(n_clicks, cam_init):
         exposure = camera_settings.settings['exposure']
         fps = camera_settings.settings['framerate']
         sensor_mode = camera_settings.settings['sensor_mode']
+        analogue_gain = camera_settings.settings['analogue_gain']
 
-        frame = picam2.still_capture(exposure, fps, sensor_mode)
+        frame = picam2.still_capture(exposure, fps, analogue_gain, sensor_mode)
 
         existing_images = glob.glob('./snap_images/snap-*.png')
         cv2.imwrite(f'./snap_images/snap-{len(existing_images)+1}.png', frame)
@@ -656,6 +647,7 @@ def upload_config(n_clicks):
             loaded_camera_settings.width = data['camera_settings']['width']
             loaded_camera_settings.height = data['camera_settings']['height']
             loaded_camera_settings.sensor_mode = data['camera_settings']['sensor_mode_ind']
+            loaded_camera_settings.analogue_gain = data['camera_settings']['analogue_gain']
 
             coord_data.xs = data['positions']['x_coordinates']
             coord_data.ys = data['positions']['y_coordinates']
@@ -671,12 +663,13 @@ def upload_config(n_clicks):
         State('exposure', 'value'),
         State('fps', 'value'),     
         State('resolution-preset', 'value'),
+        State('analogue_gain', 'value'),
         State('total-time-points', 'value'),
         State('acq-length', 'value'),
         State('each-time-limit', 'value'),
         State('acquire-path-input', 'value'),
     ])
-def save_config(n_clicks, exposure, fps, resolution, timepoints, interval, length, path):
+def save_config(n_clicks, exposure, fps, resolution, analogue_gain, timepoints, interval, length, path):
     if n_clicks is None:
         return dash.no_update
     else:
@@ -698,7 +691,8 @@ def save_config(n_clicks, exposure, fps, resolution, timepoints, interval, lengt
                 'framerate': fps,
                 'width': width,
                 'height': height,
-                'sensor_mode_ind': resolution
+                'sensor_mode_ind': resolution,
+                'analogue_gain': analogue_gain
             },
             'positions': {
                 'x_coordinates': coord_data.xs,
@@ -737,13 +731,14 @@ def save_config(n_clicks, exposure, fps, resolution, timepoints, interval, lengt
         State('exposure', 'value'),
         State('fps', 'value'),     
         State('resolution-preset', 'value'),
+        State('analogue_gain', 'value'),
         State('acquire-path-input', 'value'),
         State('xy_coords', 'data'),
         State('acquisition-number', 'value'),
         State('light-auto-dimming', 'value')
     ],
 )
-def acquire(n_clicks, cam_init, timepoints, length, time, exposure, fps, resolution, user_path, xy_data, acq_num, light_auto_dim):
+def acquire(n_clicks, cam_init, timepoints, length, time, exposure, fps, resolution, analogue_gain, user_path, xy_data, acq_num, light_auto_dim):
     if n_clicks is None:
         return trigger, False, False
     else:
@@ -766,7 +761,8 @@ def acquire(n_clicks, cam_init, timepoints, length, time, exposure, fps, resolut
                 'framerate': camera_settings.get('framerate'),
                 'width': width,
                 'height': height,
-                'sensor_mode_ind': resolution
+                'sensor_mode_ind': resolution,
+                'analogue_gain': analogue_gain
             },
             'positions': {
                 'x_coordinates': coord_data.xs,
@@ -788,7 +784,7 @@ def acquire(n_clicks, cam_init, timepoints, length, time, exposure, fps, resolut
 
         acquisition.acquire(
             folder=user_path, positions=acq_num, timepoints=timepoints, interval=length, capture_length=time, # Acquisition parameters
-            exposure=camera_settings.get('exposure'), fps=camera_settings.get('framerate'), sensor_mode=camera_settings.get('sensor_mode'), # Camera settings
+            camera=picam2, exposure=camera_settings.get('exposure'), fps=camera_settings.get('framerate'), sensor_mode=camera_settings.get('sensor_mode'), analogue_gain=camera_settings.get('analogue_gain'), # Camera settings
             stage=stage, leds=leds, light_auto_dim=light_auto_dim, # Hardware instances and settings
             exp_log=experimental_log, # Ancillary instances
             xyz_coords=coord_data, # XYZ coordinates
@@ -900,8 +896,6 @@ def home_xy_stage(n_clicks):
     if n_clicks:
         stage.setOrigin()
         time.sleep(2)
-        x,y,z = stage.grabXY()
-        relative.start(x=x, y=y, z=z)
         return trigger
     else:
         return dash.no_update
@@ -920,6 +914,8 @@ class Nclick:
     def __init__(self):
         self.n_clicks = 0
 
+mag_fval_map = {0.01: 10, 0.1: 100, 1: 500, 10: 1000}
+
 ldu_xy_clicks = Nclick()
 @app.callback(
     output=Output('left-diag-xy-up-div', 'children'),
@@ -930,7 +926,7 @@ def left_diag_up_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > ldu_xy_clicks.n_clicks:
             relative.update(x=-mag, y=-mag)
-            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F{mag_fval_map[mag]}\n'))
             ldu_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -947,7 +943,7 @@ def up_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > up_xy_clicks.n_clicks:
             relative.update(y=-mag)
-            stage.xyz.write(str.encode(f'G0 Y{relative.y}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 Y{relative.y}F{mag_fval_map[mag]}\n'))
             up_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -964,7 +960,7 @@ def right_diag_up_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > rdu_xy_clicks.n_clicks:
             relative.update(x=mag, y=-mag)
-            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F{mag_fval_map[mag]}\n'))
             rdu_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -981,7 +977,7 @@ def left_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > left_xy_clicks.n_clicks:
             relative.update(x=-mag)
-            stage.xyz.write(str.encode(f'G0 X{relative.x}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 X{relative.x}F{mag_fval_map[mag]}\n'))
             left_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -998,7 +994,7 @@ def right_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > right_xy_clicks.n_clicks:
             relative.update(x=mag)
-            stage.xyz.write(str.encode(f'G0 X{relative.x}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 X{relative.x}F{mag_fval_map[mag]}\n'))
             right_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -1015,7 +1011,7 @@ def left_diag_down_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > ldd_xy_clicks.n_clicks:
             relative.update(x=-mag, y=mag)
-            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F{mag_fval_map[mag]}\n'))
             ldd_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -1032,7 +1028,7 @@ def down_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > down_xy_clicks.n_clicks:
             relative.update(y=mag)
-            stage.xyz.write(str.encode(f'G0 Y{relative.y}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 Y{relative.y}F{mag_fval_map[mag]}\n'))
             down_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -1049,7 +1045,7 @@ def right_diag_down_xy(n_clicks, mag):
     if n_clicks:
         if n_clicks > rdd_xy_clicks.n_clicks:
             relative.update(x=mag, y=mag)
-            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 X{relative.x}Y{relative.y}F{mag_fval_map[mag]}\n'))
             rdd_xy_clicks.n_clicks += 1
             return trigger
         else:
@@ -1067,7 +1063,7 @@ def up_z(n_clicks, mag):
     if n_clicks:
         if n_clicks > up_z_clicks.n_clicks:
             relative.update(z=mag)
-            stage.xyz.write(str.encode(f'G0 Z{relative.z}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 Z{relative.z}F{mag_fval_map[mag]}\n'))
             up_z_clicks.n_clicks += 1
             return trigger
         else:
@@ -1084,7 +1080,7 @@ def down_z(n_clicks, mag):
     if n_clicks:
         if n_clicks > down_z_clicks.n_clicks:
             relative.update(z=-mag)
-            stage.xyz.write(str.encode(f'G0 Z{relative.z}F1000\n'))
+            stage.xyz.write(str.encode(f'G0 Z{relative.z}F{mag_fval_map[mag]}\n'))
             down_z_clicks.n_clicks += 1
             return trigger
         else:
@@ -1112,18 +1108,18 @@ def grab_xy(n_clicks, data):
                 data = []
                 row_id = 0
 
-            if x in coord_data.xs and y in coord_data.ys and z in coord_data.zs:
-                return dbc.Alert('Position already exists in table below.', color='danger', dismissable=True)
-            else:
-                label=f'Pos{row_id}'
-                coord_data.xs.append(x)
-                coord_data.ys.append(y)
-                coord_data.zs.append(z)
-                coord_data.labels.append(label)
-                
-                coord_data.set_current(x,y,z,label)
+            #if x in coord_data.xs and y in coord_data.ys and z in coord_data.zs:
+            #    return dbc.Alert('Position already exists in table below.', color='danger', dismissable=True)
+            #else:
+            label=f'Pos{row_id}'
+            coord_data.xs.append(x)
+            coord_data.ys.append(y)
+            coord_data.zs.append(z)
+            coord_data.labels.append(label)
+            
+            coord_data.set_current(x,y,z,label)
 
-                return trigger
+            return trigger
         else:
             trigger
     else:
@@ -1375,7 +1371,7 @@ import sys
 
 def teardown():
     """Teardown helper for shutting down various class instances."""
-    camera.close()
+    picam2.close()
     if stage:
         stage.close()
     if leds:
